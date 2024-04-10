19b5f77e864cebb1b2a970f4f6ff778f97575caf
Paulo C. Gómez Carmiol <98922137+misproyectosweb@users.noreply.github.com>
2024-04-09 19:43:21 -0600

    Actualización archivo index.php

--- index.php
+++ index.php
@@ -11,9 +11,7 @@
         <link rel="shortcut icon" type="image/x-icon" href="imagenes/iconotools.png"/>
                 
         <!-- Utilizando google fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@300;400&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@300;400&display=swap" rel="stylesheet">        
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 
         <!-- Utilizando Bootstrap 4 -->
